d5aa34ab958bc7a0a9a966351ce5ebb719362e53
Juergen <juergen@jschweiss.de>
2017-11-05 14:02:20 +0100

    final version
    Added Timer-Interrupt for TX3-LaCrosse-protocol on a 433MHz-TX-module (stx882)

---
+++ iAQ_NANO_328 - GetIAQ.ino
@@ -0,0 +1,219 @@
+//#include <IAQ2000\I2Cdev.h>
+//#include <IAQ2000\IAQ2000.h>
+#include <Wire.h>
+
+#define iaqaddress 0x5A
+
+uint16_t  predict;
+uint8_t   statu;
+int32_t   resistance;
+uint16_t  tvoc;
+
+
+// sensor readings structure after conversion and derivation
+struct sensorsT 
+{
+    // iAQ Core-C
+    byte AQ_status;           // 0-ok,1-busy,16-warming up,128-error
+    unsigned int CO2eqP;      // predicted CO2eq ppm
+    unsigned long Resistance; // sensor resistance ohms
+    unsigned int Tvoc;        // predicted TVOCeq ppb
+                              // HIH6131
+    byte RhT_status;          // 0-normal,1-stale data (already fetched), 2-in CMD mode, 3-n/u
+    float Rh;                 // relative humidity
+    float Temp;               // temperature
+};
+typedef struct sensorsT sensors;
+
+sensors sensor;
+
+//--- prototypes
+void readAllBytes();
+void checkStatus();
+int64_t get_timestamp_us();
+
+//--------------------------------------------------
+
+int64_t get_timestamp_us()
+{
+    return (int64_t)millis() * 1000;
+}
+//--------------------------------------------------
+void checkStatus()
+{
+    if (sensor.AQ_status == 0x10)
+    {
+        Serial.println("Warming up...");
+    }
+    else if (sensor.AQ_status == 0x00)
+    {
+        Serial.println("Ready");
+    }
+    else if (sensor.AQ_status == 0x01)
+    {
+        Serial.println("Busy");
+    }
+    else if (sensor.AQ_status == 0x80)
+    {
+        Serial.println("Error");
+    }
+    else
+        Serial.println("No Status, check module");
+}
+//--------------------------------------------------
+void setup()
+{
+    //  PD1 = SDA = D2
+    //  PD0 = SCL = D3
+
+    pinMode(LED_BUILTIN, OUTPUT);
+    digitalWrite(LED_BUILTIN, LOW); 
+
+    Wire.begin();
+
+    Serial.begin(115200);
+
+    while (!Serial)
+    {
+        ; // wait for serial port to connect. Needed for native USB
+        yield();
+    }
+
+    delay(500);
+
+    Serial.println("*** Running now.");
+    Serial.println("Resistance, Status, TVOC, CO2eqPpm");
+}
+//--------------------------------------------------
+void loop()
+{
+    delay(200);
+
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    
+    
+   /* readAllBytes();
+    Serial.println("ReadAllBytes done.");
+
+    checkStatus();
+    Serial.println("CheckStatus done.");
+
+    Serial.println();
+    Serial.print("CO2:");
+    Serial.print(predict);
+    Serial.print(", Status:");
+    Serial.print(statu, HEX);
+    Serial.print(", Resistance:");
+    Serial.print(resistance);
+    Serial.print(", TVoC:");
+    Serial.println(tvoc);*/
+    
+
+    getAQ();
+
+    checkStatus();
+
+    int64_t timestamp = get_timestamp_us(); 
+
+    Serial.print("[");
+    Serial.print(timestamp / 1e6);
+    Serial.print("] \t");
+   
+   // Serial.println("Resistance, Status, TVOC, CO2eqPpm"); 
+    Serial.print(sensor.Resistance);
+    Serial.print(" , ");
+
+    Serial.print(sensor.AQ_status);
+    Serial.print(" , ");
+
+    Serial.print (sensor.Tvoc);
+    Serial.print(" , ");
+
+    Serial.print(sensor.CO2eqP);
+    Serial.println("");
+        
+    digitalWrite(LED_BUILTIN, HIGH);
+
+   /* for (uint8_t i=0;i<4; i++)
+    {
+        _delay_ms (500);
+        yield();
+    }*/
+
+    delay(200);
+
+    digitalWrite(LED_BUILTIN, LOW);
+}
+//--------------------------------------------------
+void readAllBytes()
+{
+    Wire.requestFrom(iaqaddress, 9);
+
+    predict = (Wire.read() << 8 | Wire.read());
+    statu = Wire.read();
+    resistance = (Wire.read() & 0x00) | (Wire.read() << 16) | (Wire.read() << 8 | Wire.read());
+    tvoc = (Wire.read() << 8 | Wire.read());
+}
+//--------------------------------------------------
+//void checkStatus()
+//{
+//    if (statu == 0x10)
+//    {
+//        Serial.println("Warming up...");
+//    }
+//    else if (statu == 0x00)
+//    {
+//        Serial.println("Ready");
+//    }
+//    else if (statu == 0x01)
+//    {
+//        Serial.println("Busy");
+//    }
+//    else if (statu == 0x80)
+//    {
+//        Serial.println("Error");
+//    }
+//    else
+//        Serial.println("No Status, check module");
+//}
+
+//------------------------------------------------------
+
+void boardTest()
+{
+    //  PD1 = SDA = D2
+    //  PD0 = SCL = D3
+
+    digitalWrite(3, HIGH);
+    digitalWrite(2, HIGH);
+    delay(1);
+    digitalWrite(3, LOW);
+    digitalWrite(2, LOW);
+    delay(1);
+
+}
+
+//--------------------------------------
+// read iAQ Core-C 
+void getAQ() 
+{
+    byte CO2predL, CO2predH, iAQstatus, reszero, res16, res8, res0, tvocH, tvocL;
+    // ask the iAQ for data
+    // NOTE: Do not communicate with the write bit set just read the bytes
+    Wire.requestFrom(iaqaddress, 9);
+    CO2predH = Wire.read();
+    CO2predL = Wire.read();
+    iAQstatus = Wire.read();
+    reszero = Wire.read();  // always zero
+    res16 = Wire.read();
+    res8 = Wire.read();
+    res0 = Wire.read();
+    tvocH = Wire.read();
+    tvocL = Wire.read();
+    // assemble the values for the structure (could be done with the reads)
+    sensor.AQ_status = iAQstatus;
+    sensor.CO2eqP = (CO2predH << 8) | CO2predL;
+    sensor.Resistance = (res16 << 16) | (res8 << 8) | res0;
+    sensor.Tvoc = (tvocH << 8) | tvocL;
+}
---
+++ iAQ_NANO_328_LaCrosse.ino
@@ -0,0 +1,220 @@
+/*
+    I2C BusClock    = 100KHz
+    TVOC im Normalbetrieb: 125... 250
+    CO2          "  :       400...2500 (?) 
+
+    --- LaCrosse-Protocol for 433 MHz-TX
+    --- transfer measured values through LaCrosse-TX2-instance
+    --- LaCrosse-object declared as static
+
+
+*/
+
+#include <Wire.h>
+#include <TimerOne\TimerOne.h>
+#include "utility/LaCrosse.h"
+
+//--- Primary Sensor ID for FHEM CUL_TX Device receiving
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#define LACROSSE_SENSOR_ID_BASIS        92      //--- results in Lacrosse instances with two cannles each for for measument data
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#define DELAY_PAUSE_PERIOD              10000  // im ms = 10s, every n seconds a TX-package (incl. 3 Repetitions)   
+#define DELAY_PAUSE_PERIOD_SEC          30     // im s = 10s 
+#define DELAY_TIMER_CYCLES              10     // im s = 10s 
+#define PIN_ENABLE_TIMESTAMP_IN_OUTPUT  10     // jumper nach Masse schaltet Timestamp in der Ausgabe aus.  
+
+
+#define iaqaddress 0x5A
+
+volatile uint16_t   predict;
+volatile uint8_t    statu;
+volatile int32_t    resistance;
+volatile uint16_t   tvoc;
+volatile bool       timer_tick = false; // 
+
+//#define VERBOSE true
+//#define USE_STATUS_OUTPUT 
+#define USE_PLOT true 
+
+//--- prototypes
+void    readAllBytes();
+void    checkStatus();
+int64_t get_timestamp_us();
+void    print_timestamp();
+
+//--------------------------------------------------------------------------------------------
+int64_t get_timestamp_us()
+{
+    //int64_t system_current_time = 0;
+    // ...
+    // Please insert system specific function to retrieve a timestamp (in microseconds)
+    // ...
+    return (int64_t)millis() * 1000;
+}
+//--------------------------------------------------------------------------------------------
+void print_timestamp()
+{
+    //--- a jumper to GND on D10 does the job! 
+    if (digitalRead(PIN_ENABLE_TIMESTAMP_IN_OUTPUT) == LOW)
+    {
+        Serial.print("[");
+        Serial.print(get_timestamp_us() / 1e6);
+        Serial.print("] ");
+    }
+}
+//----------------------------------------------------------------------------------------------
+void timer_callback()
+{
+    if (timer_tick == true)  return ;  //ignore timer, loop is occupied! 
+    timer_tick = true;  //--- do main job in loop()
+}
+//----------------------------------------------------------------------------------------------
+void executeLaCrosseSend()
+{
+    static uint8_t counter = 0;
+
+    counter++;
+
+    if (digitalRead(PIN_ENABLE_TIMESTAMP_IN_OUTPUT) == LOW && counter > DELAY_TIMER_CYCLES)
+    {
+        print_timestamp();  Serial.println(F("Timer-Interrupt!"));
+    }
+
+    if (counter > DELAY_TIMER_CYCLES)
+    {
+        counter = 0;
+
+        //*********************************************
+        //--- LaCrosse-Protocol for 433 MHz-TX 
+        //--- transfer measured values through LaCrosse-TX2-instance
+        //--- LaCrosse-object declared as static
+
+        /*Serial.print(predict); Serial.print(",");
+        Serial.print(resistance); Serial.print(",");
+        Serial.println(tvoc);*/
+
+        LaCrosse.bSensorId = LACROSSE_SENSOR_ID_BASIS;
+        
+        float co2Reading  = predict / 10.0;     //--- CO2-reading adapts iAQ-CO2-value for protocol needs of LaCrosse-TX3
+        float tvocReading = tvoc / 10.0; 
+
+        print_timestamp(); Serial.print("CO2: "); Serial.print(co2Reading); Serial.print("\t TVOC: \t"); Serial.println(tvocReading);
+
+        LaCrosse.t = co2Reading;         //--- alias temperature;  
+        LaCrosse.h = tvocReading;    //--- alias humidity;  
+
+        //--- send CO2 reading in PPM as "xxx.xx" value, regarding TX3-protocol restrictions to "temperature"
+        LaCrosse.sendTemperature();
+        LaCrosse.sleep(1);        /* 1 second, no power-reduction! see impact on powersave */
+        
+        //--- send TVOV-reading as "xx.xx" value, regarding TX3-protocol restrictions to "humidity".
+        LaCrosse.sendHumidity();        
+        LaCrosse.sleep(1);        /* 1 second delay, no power-reduction! */
+    }
+}
+//----------------------------------------------------------------------------------------------
+void setup()
+{
+    //  PD1 = SDA = D2
+    //  PD0 = SCL = D3
+
+    Serial.begin(115200);
+
+    while (!Serial)
+    {
+        ; // wait for serial port to connect. Needed for native USB
+        yield(); // reserve for ESP8266
+    }
+
+    //Timer1.initialize((unsigned long) DELAY_PAUSE_PERIOD_SEC * 1000 * 1000); // yS in Sekunden
+    Timer1.initialize((unsigned long)30000000); // yS in Sekunden
+    Timer1.attachInterrupt(timer_callback);
+
+    Serial.println("*** Running.");
+    Serial.println("");
+    Serial.println("CO2 [ppm] , resistance, TVOC\n");
+    
+    Wire.begin(iaqaddress);
+}
+//----------------------------------------------------------------------------------------------
+void loop()
+{
+    readAllBytes();
+    #ifdef VERBOSE
+        Serial.println("ReadAllBytes done.");
+    #endif
+
+    #ifdef USE_STATUS_OUTPUT
+        checkStatus();
+    #endif 
+
+    #ifdef VERBOSE
+        Serial.println("CheckStatus done.");
+    #endif
+
+
+#ifdef USE_PLOT
+        Serial.print(predict); Serial.print(",");
+        Serial.print(resistance); Serial.print(",");
+        Serial.println(tvoc);
+#else
+    Serial.println();
+    Serial.print("CO2:");
+    Serial.print(predict);
+    Serial.print(", Status:");
+    Serial.print(statu, HEX);
+    Serial.print(", Resistance:");
+    Serial.print(resistance);
+    Serial.print(", TVoC:");
+    Serial.println(tvoc);
+#endif
+
+    if (timer_tick)
+    {
+        executeLaCrosseSend();
+        timer_tick = false;     // reset timer-flag. 
+    }
+    
+    delay(2000);
+
+}
+//----------------------------------------------------------------------------------------------
+void readAllBytes()
+{
+    Wire.requestFrom(iaqaddress, 9);
+
+    predict = (Wire.read() << 8 | Wire.read());
+    statu = Wire.read();
+    resistance = (Wire.read() & 0x00) | (Wire.read() << 16) | (Wire.read() << 8 | Wire.read());
+    tvoc = (Wire.read() << 8 | Wire.read());
+}
+//----------------------------------------------------------------------------------------------
+void checkStatus()
+{
+    if (statu == 0x10)
+    {
+        Serial.println("Warming up...");
+    }
+    else if (statu == 0x00)
+    {
+        Serial.println("Ready");
+    }
+    else if (statu == 0x01)
+    {
+        Serial.println("Busy");
+    }
+    else if (statu == 0x04)
+    {
+        //--- kommt in der Warmlaufphase ... steht nicht in der Doku!
+        Serial.println("BurnIn?");
+    }
+    else if (statu == 0x80)
+    {
+        Serial.println("Error");
+    }
+    else
+        Serial.println("No Status, check module");
+}
+//----------------------------------------------------------------------------------------------
+// <eot>
+//----------------------------------------------------------------------------------------------
